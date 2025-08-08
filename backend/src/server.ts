2f98b42ddd4b449bbcb15a12bc8da3464764e625
RiadOliveira <riad.oliveira@hotmail.com>
2025-08-08 03:16:47 -0300

    Backend dockerfile added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import { build } from './app.ts';
 const app = await build();
 
 try {
-  await app.listen({ port: 3000 });
+  await app.listen({ port: 3000, host: '0.0.0.0' });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
