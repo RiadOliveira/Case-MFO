b01d3ebfcaba7acf471c1470aeea735a063c70dd
RiadOliveira <riad.oliveira@hotmail.com>
2025-08-08 19:06:34 -0300

    SImulationEvent model added to schema.prisma

---
+++ backend/prisma/migrations/20250808220619_simulation_events/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "public"."simulation_events" (
+    "id" TEXT NOT NULL,
+    "type" "public"."EventType" NOT NULL,
+    "value" DECIMAL(65,30) NOT NULL,
+    "frequency" "public"."EventFrequency" NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "simulationId" TEXT NOT NULL,
+
+    CONSTRAINT "simulation_events_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."simulation_events" ADD CONSTRAINT "simulation_events_simulationId_fkey" FOREIGN KEY ("simulationId") REFERENCES "public"."simulations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
